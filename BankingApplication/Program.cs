4ed550398688da7d863cf341b264d994b089c8e3
George Wanderson <georgewanderson07@gmail.com>
2023-05-21 14:31:38 -0300

    Add project files.

---
+++ BankingApplication/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BankingApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello!");
+        }
+    }
+}
